e211095a4639789c21d13066b69961e33791b1e1
Maria Bladé <mariablade2000+git@gmail.com>
2022-10-05 19:11:07 +0200

    responsive navbar has problems and fetch needs to be fixed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,53 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+// <div class="topnav" id="myTopnav">
+//   <a href="#home" class="active">Home</a>
+//   <a href="#news">News</a>
+//   <a href="#contact">Contact</a>
+//   <a href="#about">About</a>
+//   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+//     <i class="fa fa-bars"></i>
+//   </a>
+// </div>
+
+// <div style="padding-left:16px">
+//   <h2>Responsive Topnav Example</h2>
+//   <p>Resize the browser window to see how it works.</p>
+// </div>
+
+
+
+let projectName;
+let projectText;
+
+
+
+const getData = async () => {
+  for (let i = 1; i < 4; i++) {
+    const id = i;
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id); //fetch carrega la info d'un enllaç
+
+    //obtenir el nom i la descripció
+    const data = await response.json();
+    console.log(data);
+    projectName = data.title;
+    projectText = data.body;
+    getMyProject(projectName, projectText, id);
+    
+  }
+}
+getData();
+//col·locar al info al HTML
+
+const getMyProject = async (name, text, id) => {
+  // l'async es perque sapiga que s haura d anar esperant
+  document.querySelector(`#descriptionCard${id}`).innerHTML = `<h4>${name}</h4><p class="headline">${text}</p>`;
+}
